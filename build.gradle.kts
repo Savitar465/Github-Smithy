0a075bcaffbf7991a5a5b51d1c66b0a073ba630a
savitar <jonasmaidana47@gmail.com>
2026-03-27 10:22:33 -0400

    Fixing build and gen code for java client

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,36 @@ plugins {
 repositories {
     mavenLocal()
     mavenCentral()
+    maven {
+        url = uri("https://smithy-lang.jfrog.io/artifactory/smithy")
+    }
 }
 
 dependencies {
     val smithyVersion: String by project
 
-    smithyCli("software.amazon.smithy:smithy-cli:$smithyVersion")
+//    smithyCli("software.amazon.smithy:smithy-cli:$smithyVersion")
 
+    // Smithy code generation plugin (note: client-codegen, NOT codegen-plugin)
+    smithyBuild("software.amazon.smithy.java:client-codegen:0.0.3")
+
+    // Runtime dependencies for the generated client
+    implementation("software.amazon.smithy.java:client-core:0.0.3")
+    implementation("software.amazon.smithy.java:aws-client-restjson:0.0.3")
     // Uncomment below to add various smithy dependencies (see full list of smithy dependencies in https://github.com/awslabs/smithy)
     // implementation("software.amazon.smithy:smithy-model:$smithyVersion")
     // implementation("software.amazon.smithy:smithy-linters:$smithyVersion")
+}
+
+afterEvaluate {
+    val clientPath = smithy.getPluginProjectionPath(smithy.sourceProjection.get(), "java-client-codegen")
+    sourceSets.main.get().java.srcDir(clientPath)
+}
+
+tasks.named("compileJava") {
+    dependsOn("smithyBuild")
+}
+
+java.sourceSets["main"].java {
+    srcDirs("model", "models")
 }
