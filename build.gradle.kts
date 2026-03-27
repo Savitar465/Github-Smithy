9483d02fa38a587e1d0396bd6ed9e2569aa3c000
Savitar <jonasmaidana47@gmail.com>
2026-03-27 01:26:16 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    `java-library`
+    id("software.amazon.smithy.gradle.smithy-jar")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    val smithyVersion: String by project
+
+    smithyCli("software.amazon.smithy:smithy-cli:$smithyVersion")
+
+    // Uncomment below to add various smithy dependencies (see full list of smithy dependencies in https://github.com/awslabs/smithy)
+    // implementation("software.amazon.smithy:smithy-model:$smithyVersion")
+    // implementation("software.amazon.smithy:smithy-linters:$smithyVersion")
+}
